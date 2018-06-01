b7f644f09cca3b414c84bd57180274dd3a717d17
Allen Roth <allenroth8888@gmail.com>
2018-06-01 10:03:22 -0400

    working so far

---
+++ reducer.js
@@ -0,0 +1,16 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+
+
+const reducer = (state = {}, action) => {
+    return state;
+}
+
+
+const store = createStore(reducer,
+  applyMiddleware(thunkMiddleware,createLogger())
+);
+
+export default store;
